19ada2fba76aeb818f411e6b031ab0f8d210db19
linnnyunmo <127164358+linnnyunmo@users.noreply.github.com>
2023-03-15 20:45:30 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,14 +1,18 @@
+#abc represent distance between different places
+#abs express absolute value
 a=-3.19
 b=-118.24
 c=116.39
 d=abs(a-b)
 e=abs(a-c)
+#compare d and e then output results
 if d<e:
         print("haining is further")
 elif e<d:
         print("los Angeles is further")
 if e==d:
         print("same")
+#compare W and Z if ture
 X=True
 Y=False
 W="X and Y"
