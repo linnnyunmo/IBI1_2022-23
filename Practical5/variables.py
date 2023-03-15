24efa44a0f1f54e54f85fe12d08c748c91019427
linnnyunmo <127164358+linnnyunmo@users.noreply.github.com>
2023-03-15 20:36:20 +0800

    Create variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,16 @@
+a=-3.19
+b=-118.24
+c=116.39
+d=abs(a-b)
+e=abs(a-c)
+if d<e:
+        print("haining is further")
+elif e<d:
+        print("los Angeles is further")
+if e==d:
+        print("same")
+X=True
+Y=False
+W="X and Y"
+Z="X or Y"
+(W,Z)
