097d39e8b8956fce8775873587627f4a6651f51c
linnnyunmo <127164358+linnnyunmo@users.noreply.github.com>
2023-03-17 15:19:29 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -7,3 +7,9 @@ while total<=100:
         n=n+1
         total=2*total
         print(n,total)
+#generation 2is4
+#generation 3is8
+#generation 4is16
+#generation 5is32
+#generation 6is64
+#generation 7is128
