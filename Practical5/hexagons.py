d020e0ec9c0eaf8887144cec3b102602c6e7d7d1
linnnyunmo <127164358+linnnyunmo@users.noreply.github.com>
2023-03-17 15:21:55 +0800

    Update hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -4,3 +4,8 @@ while n<6:
         h = 2*n*(2*n-1)/2
         print(h)
         n+=1
+#the first graph is 1
+#the second graph is 6
+#the third graph is 15
+#the forth graph is 28
+#the fifth graph is 45
