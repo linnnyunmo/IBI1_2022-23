4175a4200df1ff55729c30f028c8667ccd50469e
linnnyunmo <127164358+linnnyunmo@users.noreply.github.com>
2023-03-22 15:42:21 +0800

    Update hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,11 +1,12 @@
 #In this range n<6 calculate the first five
+#h represents the total number
+#then calculate the representation of h
+#Get the first five results
 n=1
+#calculte the the first five results
 while n<6:
         h = 2*n*(2*n-1)/2
+#calculate the formula and get results
         print(h)
+#It can always be calculated
         n+=1
-#the first graph is 1
-#the second graph is 6
-#the third graph is 15
-#the forth graph is 28
-#the fifth graph is 45
