5fba2090dd5bcac7044e3a4c84ba3b9bafc7fae2
linnnyunmo <127164358+linnnyunmo@users.noreply.github.com>
2023-03-22 16:28:42 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,8 +1,12 @@
+#Calculate the difference inabc using the formula and compare the magnitude
+#We know which is farther away aftercomparing the difference represented by d and e
+#Get result
 #abc represent distance between different places
 #abs express absolute value
 a=-3.19
 b=-118.24
 c=116.39
+#d and e represent the distance 
 d=abs(a-b)
 e=abs(a-c)
 #compare d and e then output results
@@ -13,10 +17,13 @@ elif e<d:
 if e==d:
         print("same")
 #compare W and Z if ture
-#haining is further
+#Get result
+#Logical comparison of W and Z Ture or False
+#X represents Ture,Y represents False
 X=True
 Y=False
-W="X and Y"
-Z="X or Y"
-print(W,Z)
-#W is False,Z is Ture
+#W represents x and y,Z represents x or y 
+W="X" and "Y"
+Z="X" or "Y"
+#Get result
+print(W,Z) 
