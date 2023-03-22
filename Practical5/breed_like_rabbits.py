44ea9a54bbf3873317fc73cff010339dca07f1a3
linnnyunmo <127164358+linnnyunmo@users.noreply.github.com>
2023-03-22 15:49:01 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,16 +1,14 @@
+#List the formula and calculate the total number of rabbits,loop through a range
 #total represents the first two rabbits
 #n represents generation
 #twice the growth rate to count 100
 #print the result
+#The first two here inthe first generation
 total=2
 n=1
+#Cycle over a total of less than 100
 while total<=100:
         n=n+1
         total=2*total
         print(n,total)
-#generation 2is4
-#generation 3is8
-#generation 4is16
-#generation 5is32
-#generation 6is64
-#generation 7is128
+# Get the results
