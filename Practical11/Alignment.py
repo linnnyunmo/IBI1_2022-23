ec0272a47c572fad3563b9b3515568956c02641c
linnnyunmo <127164358+linnnyunmo@users.noreply.github.com>
2023-04-27 16:36:16 +0800

    Create Alignment.py

---
+++ Practical11/Alignment.py
@@ -0,0 +1,85 @@
+# open these sequence file
+human = open('ACE2_human.fa')
+mouse = open('ACE2_mouse.fa')
+cat = open('ACE2_cat.fa')
+# Determine the sequences in these files,read sequences
+human_sequence = human.readlines()
+mouse_sequence = mouse.readlines()
+cat_sequence = cat.readlines()
+# Start reading from the first line of the amino acid sequence, this is the beginning of the amino acid sequence
+human_seq = human_sequence[1].strip()
+mouse_seq = mouse_sequence[1].strip()
+cat_seq = cat_sequence[1].strip()
+
+# Define a function to compare the two different sequences
+def percentage(seq1, seq2):
+    # set initial distance as zero
+    edit_distance = 0
+    # compare each amino acid
+    for i in range(len(seq1)):
+        if seq1[i] == seq2[i]:
+            # add a score 1 if amino acids are different
+            edit_distance += 1
+    percentage = edit_distance / len(seq1) * 100  # Calculate the percentage of similarity
+    return percentage
+# Calculate the percentage of similarity between different sequences
+print('The percentage of identical amino acids between cat and human is:',percentage(cat_seq, human_seq), end='')
+print('%')
+print('The percentage of identical amino acids between cat and mouse is:',percentage(cat_seq, mouse_seq), end='')
+print('%')
+print('The percentage of identical amino acids between mouse and human is:',percentage(mouse_seq, human_seq), end='')
+print('%')
+# This is the amino acid list
+amino_acid = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X']
+# The BLOSUM62 matrix
+matrix = [[ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0],
+    [-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1],
+    [-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1],
+    [-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1],
+    [ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2],
+    [-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1],
+    [-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1],
+    [ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1],
+    [-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1],
+    [-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1],
+    [-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1],
+    [-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1],
+    [-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1],
+    [-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1],
+    [-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2],
+    [ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0],
+    [ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0],
+    [-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2],
+    [-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1],
+    [ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1],
+    [-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1],
+    [-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1],
+    [ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1]]
+
+# Define a function to calculate the sequence score
+def BLOSUM_score(seq1, seq2):
+    score = 0
+    # Determine the location of amino_acid by x,y
+    for i in range(len(seq1)):
+        for l in range(len(amino_acid)):
+            if seq1[i] == amino_acid[l]:
+                x = l
+                break
+        for l in range(len(amino_acid)):
+            if seq2[i] == amino_acid[l]:
+                y = l
+                break
+        # The BLOSUM score between the two sequences
+        score = score + matrix[x][y]
+    return score
+
+
+# Calculate the score between two different sequences
+score1= BLOSUM_score(mouse_seq, human_seq)
+score2 = BLOSUM_score(mouse_seq, cat_seq)
+score3 = BLOSUM_score(human_seq, cat_seq)
+
+# Print the score between two sequences in these three comparison
+print('The score between mouse and human is:', score1)
+print('The score between mouse and cat is:', score2)
+print('The score between cat and human is:', score3)
